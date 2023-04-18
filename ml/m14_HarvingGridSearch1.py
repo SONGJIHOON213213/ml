c079216b36dce1dc6ae675ff21b40b81d729a0af
SONGJIHOON213213 <126453984+SONGJIHOON213213@users.noreply.github.com>
2023-04-18 16:38:25 +0900

    0418
    0418

--- ml/m14_HarvingGridSearch1.py
+++ ml/m14_HarvingGridSearch1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from tensorflow.keras.layers import Dense
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris,load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold,cross_val_score
 from sklearn.metrics import accuracy_score
@@ -11,6 +11,7 @@ import time
 import pandas as pd
 #1.데이터
 x , y = load_iris(return_X_y=True)
+x , y = load_digits(return_X_y=True)
 
 x_train,x_test,y_train,y_test = train_test_split(
     x, y, shuffle=True, random_state=337, test_size=0.2, stratify=y)
@@ -18,15 +19,16 @@ x_train,x_test,y_train,y_test = train_test_split(
 n_split = 3
 kfold = KFold(n_splits=n_split,shuffle=True,random_state=337)
 
-parameters = [
- {"C":[1,10,100,1000],"kernel":['linear'],'degree':[3,4,5]}, #12
- {'C':[1,10,100],"kernel":['rbf','linear'],'gamma':[0.01,0.0001]},    #12
- {'C':[1,10,1000],"kernel":['sigmoid'],
-  'gamma':[0.01,0.001,0.0001],"degree":[3,4]},                #24
- {'C':[0.1,1],'gamma':[1,10]},
+parameter = [
+    {'n_estimators':[100, 200], 'max_depth':[6,8,10], 'min_samples_leaf':[1,10]}, 
+    {'max_depth':[6,8,10,12], 'min_samples_leaf':[3,5,7,10]},
+    {'min_samples_leaf':[3,5,7,10], 'min_samples_split':[2,3,5,10]},
+    {'min_samples_split':[2,3,5,10]},
+    {'n_jobs':[-1, 2, 4], 'min_samples_split':[2,3,5,10]}
 ]
 #2.모델
-model = HalvingGridSearchCV(SVC(),parameters,
+# model = GridSearchCV(SCV())
+model = HalvingGridSearchCV(SVC(),parameter,
                      cv=5,
                     #  n_iter=10,
                      verbose=1,
