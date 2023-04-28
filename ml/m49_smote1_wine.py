45fb1d386a7bcda8c7bad94aaf5221275467901f
SONGJIHOON213213 <126453984+SONGJIHOON213213@users.noreply.github.com>
2023-04-28 17:44:30 +0900

    0428
    0428

--- ml/m49_smote1_wine.py
+++ ml/m49_smote1_wine.py
@@ -4,10 +4,57 @@ from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,f1_score
 from imblearn.over_sampling import SMOTE
+from tensorflow.python.keras.models import Sequential, Model, load_model
+import numpy as np
+from tensorflow.python.keras.layers import Dense, Input, Dropout
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.callbacks import EarlyStopping
+import tensorflow as tf
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import RobustScaler, MaxAbsScaler
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder
+#1. 데이터
+path = 'c:/study/_data/wine/'  
+train_csv = pd.read_csv(path + 'train.csv', 
+                        index_col=0) 
 
-datasets = load_wine()
-x = datasets.data
-y = datasets['target']
+
+print(train_csv)
+print(train_csv.shape) #출력결과 (10886, 11)
+
+test_csv = pd.read_csv(path + 'test.csv',
+                       index_col=0) 
+              
+print(test_csv)       
+print(test_csv.shape)  
+# Label Encoding & One-hot Encoding
+enc = LabelEncoder()
+enc.fit(train_csv['type'])
+train_csv['type'] = enc.transform(train_csv['type'])
+test_csv['type'] = enc.transform(test_csv['type'])
+
+ohe = OneHotEncoder()
+y = train_csv['quality'].values
+y = y.reshape(-1, 1)
+y = ohe.fit_transform(y).toarray()
+
+# 데이터 전처리
+x = train_csv.drop(['quality'], axis=1)
+scaler = MinMaxScaler()
+scaler.fit(x)
+x = scaler.transform(x)
+
+# test_csv를 DataFrame으로 변환
+test_csv_df = pd.DataFrame(test_csv, columns=train_csv.columns[1:])
+
+# test_csv 전처리
+test_csv_df = scaler.transform(test_csv_df)
+
 print(x.shape,y.shape) 
 print(np.unique(y,return_counts=True))
 print(pd.Series(y).value_counts().sort_index())
