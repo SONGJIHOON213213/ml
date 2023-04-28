5ed8dc3bb31bb465fe53b3314a62dbf461692cad
SONGJIHOON213213 <126453984+SONGJIHOON213213@users.noreply.github.com>
2023-04-28 18:08:36 +0900

    0428
    0428

--- ml/m49_smote1_wine.py
+++ ml/m49_smote1_wine.py
@@ -1,38 +1,14 @@
-import numpy as np
-import pandas as pd 
-from sklearn.datasets import load_wine
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score,f1_score
-from imblearn.over_sampling import SMOTE
-from tensorflow.python.keras.models import Sequential, Model, load_model
-import numpy as np
-from tensorflow.python.keras.layers import Dense, Input, Dropout
-from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.callbacks import EarlyStopping
-import tensorflow as tf
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.preprocessing import RobustScaler, MaxAbsScaler
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import OneHotEncoder
-from xgboost import XGBClassifier
-from sklearn.preprocessing import LabelEncoder
-#1. 데이터
-path = 'c:/study/_data/wine/'  
-train_csv = pd.read_csv(path + 'train.csv', 
-                        index_col=0) 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+from imblearn.over_sampling import SMOTE
 
+path = 'c:/study/_data/wine/'
+train_csv = pd.read_csv(path + 'train.csv', index_col=0)
+test_csv = pd.read_csv(path + 'test.csv', index_col=0)
 
-print(train_csv)
-print(train_csv.shape) #출력결과 (10886, 11)
-
-test_csv = pd.read_csv(path + 'test.csv',
-                       index_col=0) 
-              
-print(test_csv)       
-print(test_csv.shape)  
-# Label Encoding & One-hot Encoding
 enc = LabelEncoder()
 enc.fit(train_csv['type'])
 train_csv['type'] = enc.transform(train_csv['type'])
@@ -43,54 +19,29 @@ y = train_csv['quality'].values
 y = y.reshape(-1, 1)
 y = ohe.fit_transform(y).toarray()
 
-# 데이터 전처리
 x = train_csv.drop(['quality'], axis=1)
 scaler = MinMaxScaler()
 scaler.fit(x)
 x = scaler.transform(x)
 
-# test_csv를 DataFrame으로 변환
 test_csv_df = pd.DataFrame(test_csv, columns=train_csv.columns[1:])
+test_csv_df = scaler.transform(test_csv_df) 
+test_csv_df = pd.DataFrame(test_csv_df, columns=train_csv.columns[1:])
 
-# test_csv 전처리
-test_csv_df = scaler.transform(test_csv_df)
+x_train, x_val, y_train, y_val = train_test_split(x, y, train_size=0.75, shuffle=True, random_state=3377, stratify=y)
 
-print(x.shape,y.shape) 
-print(np.unique(y,return_counts=True))
-print(pd.Series(y).value_counts().sort_index())
+smote = SMOTE(random_state=3377)
+x_train, y_train = smote.fit_resample(x_train, y_train) 
 
-x_train,x_test,y_train,y_test=train_test_split(
-    x,y,train_size=0.75,shuffle=True,random_state=3377,stratify=y
-) 
-
-from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier(random_state=3377)
+model.fit(x_train, y_train)
 
-#3.훈련
-model.fit(x_train,y_train)
+y_pred = model.predict(x_val)
+score = model.score(x_val, y_val)
 
-#4.평가,예측
-y_predict = model.predict(x_test)
-score = model.score(x_test,y_test)
-print('Before SMOTE')
-print('model.score: ', score) 
-print('accuracy_score : ',accuracy_score(y_test,y_predict))
-print('f1_score(macro) : ', f1_score(y_test,y_predict,average='macro'))
-print('f1_score(micro) : ', f1_score(y_test,y_predict,average='micro'))
-
-print("=====================================SMOTE 적용후========")
-smote = SMOTE(random_state=337,k_neighbors=8)
-x_train,y_train = smote.fit_resample(x_train.copy(),y_train.copy())
-
-#3.훈련
-model.fit(x_train,y_train)
-
-#4.평가,예측
-y_predict = model.predict(x_test)
-score = model.score(x_test,y_test)
 print('After SMOTE')
-print('model.score: ', score) 
-print('accuracy_score : ',accuracy_score(y_test,y_predict))
-print('f1_score(macro) : ', f1_score(y_test,y_predict,average='macro'))
-print('f1_score(micro) : ', f1_score(y_test,y_predict,average='micro'))
-print(pd.Series(y_train).value_counts().sort_index())
+print('model.score: ', score)
+print('accuracy_score: ', accuracy_score(y_val, y_pred))
+print('f1_score(macro): ', f1_score(y_val, y_pred, average='macro'))
+print('f1_score(micro): ', f1_score(y_val, y_pred, average='micro'))
+print(pd.Series(y_train).value_counts().sort_index()) 
