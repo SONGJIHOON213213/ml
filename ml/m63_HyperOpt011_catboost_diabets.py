f07dce2b90641d33806bdee2773de864e149be83
SONGJIHOON213213 <126453984+SONGJIHOON213213@users.noreply.github.com>
2023-05-03 22:25:34 +0900

    0503
    0503

--- ml/m63_HyperOpt011_catboost_diabets.py
+++ ml/m63_HyperOpt011_catboost_diabets.py
@@ -1,14 +1,24 @@
-from sklearn.datasets import load_diabetes
+import pandas as pd
+from sklearn.datasets import load_breast_cancer, load_diabetes, load_iris
+from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score
-from catboost import CatBoostRegressor
-from hyperopt import hp, fmin, Trials, STATUS_OK, tpe
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler, RobustScaler 
+from xgboost import XGBClassifier, XGBRegressor
+from catboost import CatBoostClassifier, CatBoostRegressor
+from bayes_opt import BayesianOptimization
+from sklearn.metrics import r2_score, mean_squared_error, accuracy_score
 import numpy as np
+from lightgbm import LGBMRegressor
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import BaggingClassifier
 import time
-
-
 # 데이터 로드 및 전처리
+#1. 데이터 
 x, y = load_diabetes(return_X_y=True)
 
 x_train, x_test, y_train, y_test = train_test_split(
@@ -20,54 +30,82 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
 
-# CatBoost 모델 학습을 위한 하이퍼파라미터 공간 정의
-catboost_space = {
-    'learning_rate': hp.uniform('learning_rate', 0.001, 1),
-    'max_depth': hp.quniform('max_depth', 3, 16, 1),
-    'subsample': hp.uniform('subsample', 0.5, 1),
-    'reg_lambda': hp.uniform('reg_lambda', 1, 10),
-    'min_data_in_leaf': hp.quniform('min_data_in_leaf', 1, 100, 1),
-    'max_bin': hp.quniform('max_bin', 100, 500, 1),
+#hyperopt----------------------------------------------------------#
+from hyperopt import hp
+from hyperopt import fmin, tpe, Trials, STATUS_OK
+
+search_space = {
+    'learning_rate' : hp.uniform('learning_rate', 0.01, 1),          
+    'depth' : hp.quniform('depth',3, 16, 1),               
+    'one_hot_max_size' : hp.quniform('one_hot_max_size',24, 64, 1),          
+    'min_data_in_leaf' : hp.quniform('min_data_in_leaf', 10, 200, 2), 
+    'bagging_temperature' : hp.uniform('bagging_temperature', 0.5, 1),
+    'random_strength' : hp.uniform('random_strength', 0.5, 1),         
+    'l2_leaf_reg' : hp.uniform('l2_leaf_reg', 0.01, 30)
 }
 
+#모델 정의 
+def cat_hamsu(search_space):
+    params = {
+        'iterations' : 10,
+        'learning_rate' : search_space['learning_rate'],
+        'depth' : int(search_space['depth']),
+        'l2_leaf_reg' : search_space['l2_leaf_reg'],
+        'bagging_temperature' : search_space['bagging_temperature'],
+        'random_strength' : search_space['random_strength'],
+        'one_hot_max_size' : int(search_space['one_hot_max_size']),
+        'min_data_in_leaf' : int(search_space['min_data_in_leaf']),
+        'task_type' : 'CPU',
+        'logging_level' : 'Silent',
+        
+    }
 
-# CatBoost 모델 학습 함수 정의
-def catboost_hamsu(params):
-    model = CatBoostRegressor(
-        n_estimators=1000,
-        learning_rate=params['learning_rate'],
-        max_depth=int(params['max_depth']),
-        subsample=params['subsample'],
-        reg_lambda=params['reg_lambda'],
-        min_data_in_leaf=int(params['min_data_in_leaf']),
-        max_bin=int(params['max_bin']),
-        random_state=337
-    )
-    model.fit(
-        x_train, y_train,
-        eval_set=[(x_test, y_test)],
-        use_best_model=True,
-        verbose=False,
-    )
+    model = CatBoostClassifier(**params)
+    model.fit(x_train, y_train)
+            #   eval_set=[(x_train, y_train), (x_test, y_test)],
+            #   eval_metric='AUC',
+            #   verbose=0,
+            #   early_stopping_rounds=50
+            #   )
     y_predict = model.predict(x_test)
-    results = r2_score(y_test, y_predict)
-    return {'loss': -results, 'status': STATUS_OK}
+    return_value = -(accuracy_score(y_test, y_predict))
+
+    return return_value
+
+trial_val = Trials()   #hist보기위해
 
 
-# 하이퍼파라미터 튜닝 및 최적값 탐색
-trials = Trials()
+best = fmin(
+    fn= cat_hamsu,                            
+    space= search_space,                        
+    algo=tpe.suggest,                           
+    max_evals=50,                               
+    trials=trial_val,                           
+    rstate = np.random.default_rng(seed=10)    
+)
 
-start_time = time.time()
-best = fmin(fn=catboost_hamsu,
-            space=catboost_space,
-            algo=tpe.suggest,
-            max_evals=100,
-            trials=trials,
-            verbose=1,
-            rstate=np.random.RandomState(337).randint(0, 1000000))
-end_time = time.time()
 
-# 결과 출력
-print("Time taken:", end_time - start_time)
-print("Best hyperparameters:", best)
-print("Best R^2 score:", -trials.best_trial['result']['loss'])
+print("best:", best)
+
+
+results = [aaa['loss'] for aaa in trial_val.results]   
+df = pd.DataFrame({
+        'learning_rate' : trial_val.vals['learning_rate'],
+        'depth' : trial_val.vals['depth'],
+        'l2_leaf_reg' : trial_val.vals['l2_leaf_reg'],
+        'bagging_temperature' : trial_val.vals['bagging_temperature'],
+        'random_strength' : trial_val.vals['random_strength'],
+        'one_hot_max_size' : trial_val.vals['one_hot_max_size'],
+        'min_data_in_leaf' : trial_val.vals['min_data_in_leaf'],
+        'results': results
+                   })
+
+print(df)
+
+
+min_row = df.loc[df['results'] == df['results'].min()]
+print("최소 행",'\n' , min_row)
+
+
+min_results = df.loc[df['results'] == df['results'].min(), 'results']
+print(min_results.values)  
